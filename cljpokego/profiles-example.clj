1be222d89e3c610d3db1c92ee1c0029e14bf7698
kvey <colton@coltonpierson.com>
2016-07-27 11:52:00 -0700

    example profiles file

---
+++ cljpokego/profiles-example.clj
@@ -0,0 +1,14 @@
+{:user
+ {:env {
+        ;; these are only necessary if you uncomment the database dependent code
+        :db-username ""
+        :db-jdbc-url (str "jdbc:postgresql:" "" )
+        :db-password ""
+        :db-database-name ""
+        :db-subname ""
+
+        :map-google-maps-key ""
+
+        :nian-accounts
+        [{:username ""
+          :password ""} ]}}}
