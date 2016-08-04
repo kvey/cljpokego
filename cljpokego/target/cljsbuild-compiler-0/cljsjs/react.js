265fda794af0bb9ffc773a867878f70ed5634625
kvey <colton@coltonpierson.com>
2016-08-04 00:31:07 -0700

    removing

--- cljpokego/target/cljsbuild-compiler-0/cljsjs/react.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.9.93 {}
-goog.provide('cljsjs.react');
-goog.require('cljs.core');
--- cljpokego/target/cljsbuild-compiler-0/reagent/interop.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 1.9.93 {}
-goog.provide('reagent.interop');
-goog.require('cljs.core');
